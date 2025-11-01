7639dc980a3644e83d086fa28a8da2566a31c14f
samplevelofy <apexlife030@gmail.com>
2025-11-01 11:08:09 +0000

    fix(tsconfig): replaced tsconfig.json with Astro's recommended strict config to resolve type definition and file not found errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-// import image from '@astrojs/image'; // <--- REMOVED: No longer importing @astrojs/image directly
 import vercel from '@astrojs/vercel';
-
+import swup from '@swup/astro';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [], // <--- MODIFIED: Removed [image()] from integrations
+  integrations: [
+    swup(),
+  ], // <--- MODIFIED: Removed [image()] from integrations
   adapter: vercel({
     imageService: true, // <--- ADDED: Enable Vercel's native image optimization service
   }),
